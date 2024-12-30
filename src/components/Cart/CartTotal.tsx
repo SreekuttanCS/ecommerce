0817b854c6522a25d463cd68f79243c60ac3bdd6
SreekuttanCS <sreekuttancs26@gmail.com>
2024-12-30 19:00:03 +0530

    Cart Updates

--- src/components/Cart/CartTotal.tsx
+++ src/components/Cart/CartTotal.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import styles from "./CartTotal.module.css";
 
-const CartTotal = () => {
+export const CartTotal = () => {
   return (
     <>
-      <div className={styles.CartTotalCard}></div>
+      <div className={styles.CartTotalCard}>
+        <h3>Price Details (2 items)</h3>
+        <div className={styles.PriceDetails}>
+          <div className={styles.TotalPrice}>
+            <h5>Total MRP</h5> <h5>₹2998.00</h5>
+          </div>
+          <div className={styles.TotalDiscount}>
+            <h5>Total Discount</h5> <h5> -₹1720.00</h5>
+          </div>
+          <div className={styles.TotalPayable}>
+            <h5>Total Payable </h5> <h5> ₹1278.00</h5>
+          </div>
+          <button className={styles.btn}>Confirm Order</button>
+        </div>
+      </div>
     </>
   );
 };
+
 export default CartTotal;
