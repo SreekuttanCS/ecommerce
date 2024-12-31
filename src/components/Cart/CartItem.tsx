6862d63fea0d04b4a7a378d235cddaf6424fef71
SreekuttanCS <sreekuttancs26@gmail.com>
2024-12-31 11:07:01 +0530

    Product card update

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -2,57 +2,53 @@ import React from "react";
 import styles from "./CartItem.module.css";
 
 const CartItem = () => {
-   return (
-      <>
-         <div className={styles.main}>
-            {" "}
-            <div className={styles.cartItemsCard}>
-               <div className={styles.prodImage}>
-                  <img src="src\assets\banner.jpg  " alt="" />
-               </div>
-               <div className={styles.prodDetails}>
-                  <button className={styles.prodDelete}>×</button>
-                  <span className={styles.productName}>
-                     Adjustable Dumbbells Set
-                  </span>
-                  <div className={styles.prodQTY}>
-                     <span className={styles.qty}>QTY: </span>
-                     <button className={styles.btn}>-</button>
-                     <span>1</span>
-                     <button className={styles.btn}>+</button>
-                  </div>
-                  <div className={styles.price}>
-                     <span className={styles.discountedPrice}> ₹7499</span>
-                     <span className={styles.originalPrice}> ₹8999</span>
-                  </div>
-                  <div className={styles.price}></div>
-               </div>
+  return (
+    <>
+      <div className={styles.main}>
+        {" "}
+        <div className={styles.cartItemsCard}>
+          <div className={styles.prodImage}>
+            <img src="src\assets\banner.jpg  " alt="" />
+          </div>
+          <div className={styles.prodDetails}>
+            <button className={styles.prodDelete}>×</button>
+            <span className={styles.productName}>Adjustable Dumbbells Set</span>
+            <div className={styles.prodQTY}>
+              <span className={styles.qty}>QTY: </span>
+              <button className={styles.btn}>-</button>
+              <span>1</span>
+              <button className={styles.btn}>+</button>
             </div>
-            <div className={styles.cartItemsCard}>
-               <div className={styles.prodImage}>
-                  <img src="src\assets\thredmill.png " alt="" />
-               </div>
-               <div className={styles.prodDetails}>
-                  <button className={styles.prodDelete}>×</button>
-                  <span className={styles.productName}>
-                     Adjustable Dumbbells Set
-                  </span>
-                  <div className={styles.prodQTY}>
-                     <span className={styles.qty}>QTY: </span>
-                     <button className={styles.btn}>-</button>
-                     <span>1</span>
-                     <button className={styles.btn}>+</button>
-                  </div>
-                  <div className={styles.price}>
-                     <span className={styles.discountedPrice}> ₹7499</span>
-                     <span className={styles.originalPrice}> ₹8999</span>
-                  </div>
-                  <div className={styles.price}></div>
-               </div>
+            <div className={styles.price}>
+              <span className={styles.discountedPrice}> ₹7499</span>
+              <span className={styles.originalPrice}> ₹8999</span>
             </div>
-         </div>
-      </>
-   );
+            <div className={styles.price}></div>
+          </div>
+        </div>
+        <div className={styles.cartItemsCard}>
+          <div className={styles.prodImage}>
+            <img src="src\assets\thredmill.png " alt="" />
+          </div>
+          <div className={styles.prodDetails}>
+            <button className={styles.prodDelete}>×</button>
+            <span className={styles.productName}>Adjustable Dumbbells Set</span>
+            <div className={styles.prodQTY}>
+              <span className={styles.qty}>QTY: </span>
+              <button className={styles.btn}>-</button>
+              <span>1</span>
+              <button className={styles.btn}>+</button>
+            </div>
+            <div className={styles.price}>
+              <span className={styles.discountedPrice}> ₹7499</span>
+              <span className={styles.originalPrice}> ₹8999</span>
+            </div>
+            <div className={styles.price}></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CartItem;
