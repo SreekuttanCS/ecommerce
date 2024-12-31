226cc65854d6f6eefd506ad9bea8eb725d61b89b
Christo-Jacob <christojacob28@gmail.com>
2024-12-31 10:55:26 +0530

    rearrange ProductCard

--- src/components/ProductShop/ProductCard.tsx
+++ src/components/ProductShop/ProductCard.tsx
@@ -4,32 +4,34 @@ import { IoIosStar } from "react-icons/io";
 import styles from "./ProductShop.module.css";
 
 const ProductCard = ({ prev }) => {
-  return (
-    <>
-      <div className={styles.individualCard}>
-        <div className={styles.prodImage}>
-          <img src={prev.image} alt="" />
-          <span className={styles.rating}>
-            {prev.rating}
-            <IoIosStar />
-          </span>
-        </div>
+   return (
+      <>
+         <div className={styles.individualCard}>
+            <div className={styles.prodImage}>
+               <img src={prev.image} alt="" />
+               <span className={styles.rating}>
+                  {prev.rating}
+                  <IoIosStar />
+               </span>
+            </div>
 
-        <span className={styles.productName}>{prev.name}</span>
+            <span className={styles.productName}>{prev.name}</span>
 
-        <div className={styles.price}>
-          <span className={styles.discountedPrice}>
-            ${prev.price.discounted}
-          </span>
-          <span className={styles.originalPrice}>${prev.price.original}</span>
-        </div>
-        <div>
-          {/* <button className={styles.remove-cart}>Remove from Cart</button> */}
-          <button className={styles.addCart}>Add to Cart</button>
-        </div>
-      </div>
-    </>
-  );
+            <div className={styles.price}>
+               <span className={styles.discountedPrice}>
+                  ₹{prev.price.discounted}
+               </span>
+               <span className={styles.originalPrice}>
+                  ₹{prev.price.original}
+               </span>
+            </div>
+            <div>
+               {/* <button className={styles.remove-cart}>Remove from Cart</button> */}
+               <button className={styles.addCart}>Add to Cart</button>
+            </div>
+         </div>
+      </>
+   );
 };
 
 export default ProductCard;
