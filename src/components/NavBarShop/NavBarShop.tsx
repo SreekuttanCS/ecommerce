4bf28d41d63960e1a1f93bb86ea63347db9a749d
SreekuttanCS <sreekuttancs26@gmail.com>
2025-01-02 10:50:23 +0530

    Changes in Individual Product ,ProductShop

--- src/components/NavBarShop/NavBarShop.tsx
+++ src/components/NavBarShop/NavBarShop.tsx
@@ -2,51 +2,49 @@ import React from "react";
 import styles from "./NavBarShop.module.css";
 
 const NavBarShop = () => {
-  return (
-    <>
-      <div className={styles.NavSection}>
-        <div className={styles.NavLeft}>
-          <img
-            src="src\assets\loder.png.webp"
-            alt="oops"
-            className={styles.logo}
-          />
-          <h3 className={styles.logoName}>FIT MAESTRO</h3>
-        </div>
-        <div className={styles.NavRight}>
-          <button className={styles.icon}>
-            <img
-              src="src\assets\search.jpeg"
-              alt="oops"
-              className={styles.img}
-            />
-          </button>
+   return (
+      <>
+         <div className={styles.NavSection}>
+            <div className={styles.NavLeft}>
+               <img
+                  src="src\assets\loder.png.webp"
+                  alt="oops"
+                  className={styles.logo}
+               />
+               <h3 className={styles.logoName}>FIT MAESTRO</h3>
+            </div>
+            <div className={styles.NavRight}>
+               <button className={styles.icon}>
+                  <img
+                     src="src\assets\search.jpeg"
+                     alt="oops"
+                     className={styles.img}
+                  />
+               </button>
 
-          <button className={styles.icon}>
-            
-            <img
-              src="src\assets\profile.jpeg"
-              alt="oops"
-              className={styles.img}
-            />
-          </button>
+               <button className={styles.icon}>
+                  <img
+                     src="src\assets\profile.jpeg"
+                     alt="oops"
+                     className={styles.img}
+                  />
+               </button>
 
-          <button className={styles.icon}>
-            <img
-              src="src\assets\heart.jpeg"
-              alt="oops"
-              className={styles.img}
-            />
-          </button>
+               <button className={styles.icon}>
+                  <img
+                     src="src\assets\heart.jpeg"
+                     alt="oops"
+                     className={styles.img}
+                  />
+               </button>
 
-            {/* <button className={styles.icon}>
+               {/* <button className={styles.icon}>
               <img src="src\assets\cart.jpeg" alt="oops" className={styles.img} />
             </button> */}
-            
-        </div>
-      </div>
-    </>
-  );
+            </div>
+         </div>
+      </>
+   );
 };
 
 export default NavBarShop;
--- src/components/ProductShop/ProductShop.tsx
+++ src/components/ProductShop/ProductShop.tsx
@@ -3,14 +3,16 @@ import { clothingProducts } from "../../product/clothingProducts";
 import { fitnessEquipment } from "../../product/fitnessProducts";
 import ProductCategory from "./ProductCategory";
 import "./ProductShop.module.css";
+import ProductIndivitualCard from "./ProductIndivitualCard";
 
 const ProductShop = () => {
-  return (
-    <>
-      <ProductCategory title="Clothing" products={clothingProducts} />
-      <ProductCategory title="Fitness" products={fitnessEquipment} />
-    </>
-  );
+   return (
+      <>
+         <ProductCategory title="Clothing" products={clothingProducts} />
+         <ProductCategory title="Fitness" products={fitnessEquipment} />
+         <ProductIndivitualCard />
+      </>
+   );
 };
 
 export default ProductShop;
