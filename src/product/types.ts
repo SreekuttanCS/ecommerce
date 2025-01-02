61e0063ae49d30fd6313109e1aac530038f36b1c
Athira-p-g <athirabkm@gmail.com>
2025-01-02 11:39:50 +0530

    changes in type of product

--- src/product/types.ts
+++ src/product/types.ts
@@ -1,16 +1,27 @@
 // Define a Price type
 export type Price = {
-   original: number;
-   discounted: number;
+  original: number;
+  discounted: number;
 };
 
 // Define a Product type
 export interface Product {
-   id: number;
-   name: string;
-   image: string;
-   price: Price;
-   category: string;
-   rating: number;
-   reviews: number;
+  id: number;
+  name: string;
+  image: string;
+  price: Price;
+  category: string;
+  rating: number;
+  reviews: number;
+  description: string;
+  specifications: {
+    color: string[];
+    fabric?: string;
+    pattern?: string;
+    neck?: string;
+    sleeve?: string;
+    size?: string[];
+  };
+  featuers: string[];
+  brand: string;
 }
